e716a39f0b2e19a5ec864dd20497eed1bc109001
ztreuse <tjzbay13@gmail.com>
2024-04-01 01:18:26 +0800

    bago

--- assets/JS/script-products.js
+++ assets/JS/script-products.js
@@ -6,3 +6,29 @@ openAbout.addEventListener("click", () => {
 openHome.addEventListener("click", () => {
   window.open("../index.html", "_self");
 });
+
+window.onload = function() {
+  var newArrivalsDiv = document.getElementById('newArrivalsLink');
+  newArrivalsDiv.style.cursor = 'pointer'; 
+  newArrivalsDiv.addEventListener('click', function() {
+    window.location.href = '#newarrivals'; 
+  });
+
+  var bestSellersH5 = document.getElementById('bestSellersLink');
+  bestSellersH5.style.cursor = 'pointer'; 
+  bestSellersH5.addEventListener('click', function() {
+    window.location.href = '#bestsellers'; 
+  });
+
+  var categoriesH5 = document.getElementById('categoriesLink');
+  categoriesH5.style.cursor = 'pointer'; 
+  categoriesH5.addEventListener('click', function() {
+    window.location.href = '#categories'; 
+  });
+
+  var specialsH5 = document.getElementById('specialsLink');
+  specialsH5.style.cursor = 'pointer'; 
+  specialsH5.addEventListener('click', function() {
+    window.location.href = '#specials'; 
+  });
+};
